46d024046940cfddcfe34cdb35bde2f0359c080e
unknown <andrew12lewis@gmail.com>
2018-11-08 17:27:24 -0800

    converted Navbar to a vue component w/same functionality

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,19 @@
 <template>
   <div id="app">
-    <nav class="navbar is-dark" role="navigation" aria-label="dropdown navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/#/" style="font-weight:bold;">
-            <span>Bali Guide</span>
-        </a>
-        <div class="navbar-burger burger" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-      </div>
-      <div class="navbar-menu" v-bind:class="{ 'is-active' : showNav }">
-          <div class="navbar-start">
-              <a class="navbar-item">
-                  <span><i class="mdi mdi-24px mdi-library-books"></i>Locations</span>
-              </a>
-              <a class="navbar-item">
-                  <span><i class="mdi mdi-24px mdi-comment-text-outline"></i>Hotels</span>
-              </a>
-              <a class="navbar-item">
-                  <span><i class="mdi mdi-24px mdi-account-multiple"></i> People</span>
-              </a>
-              <div class="navbar-item has-dropdown is-hoverable" @click="showSubMenu()">
-                  <a class="navbar-link"><span><i class="mdi mdi-24px mdi-view-list"></i> Restaurants</span></a>
-                  <div class="navbar-dropdown is-hidden-touch">
-                      <a class="navbar-item" href=""><span><i class="mdi mdi-24px mdi-star"></i> Favorites</span></a>
-                  </div>
-              </div>
-          </div>
-          <div class="navbar-end">
-            <a class="button is Danger" @click="login()">
-                    Login with Google
-            </a>
-            <a class="button is-primary" @click="logout()">
-                    Logout
-            </a>
-          </div>
-      </div>
-    </nav>
+    <Navbar class="Navbar"/>
     <router-view class="main container"/>
   </div>
 </template>
 
 <script>
 
-
-import { mapActions, mapState } from 'vuex';
+import Navbar from './components/Navbar.vue';
 
 export default {
-  computed: mapState('auth', ['user', 'isLoggedIn']),
-  methods: mapActions('auth', ['login', 'logout']),
+
+  components: {
+    Navbar,
+  },
 
 };
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <nav class="navbar is-dark" role="navigation" aria-label="dropdown navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/#/" style="font-weight:bold;">
+            <span>Bali Guide</span>
+        </a>
+        <div class="navbar-burger burger"
+        onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+      </div>
+      <div class="navbar-menu" v-bind:class="{ 'is-active' : showNav }">
+          <div class="navbar-start">
+              <a class="navbar-item">
+                  <span><i class="mdi mdi-24px mdi-library-books"></i>Locations</span>
+              </a>
+              <a class="navbar-item">
+                  <span><i class="mdi mdi-24px mdi-comment-text-outline"></i>Hotels</span>
+              </a>
+              <a class="navbar-item">
+                  <span><i class="mdi mdi-24px mdi-account-multiple"></i> People</span>
+              </a>
+              <div class="navbar-item has-dropdown is-hoverable"
+               @click="showSubMenu()">
+                  <a class="navbar-link"><span><i class="mdi mdi-24px mdi-view-list">
+                    </i> Restaurants</span></a>
+                  <div class="navbar-dropdown is-hidden-touch">
+                      <a class="navbar-item" href="">
+                        <span><i class="mdi mdi-24px mdi-star"></i> Favorites</span></a>
+                  </div>
+              </div>
+          </div>
+          <div class="navbar-end">
+            <a class="button is Danger" @click="login()">
+                    Login with Google
+            </a>
+            <a class="button is-primary" @click="logout()">
+                    Logout
+            </a>
+          </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+
+export default {
+  computed: mapState('auth', ['user', 'isLoggedIn']),
+  methods: mapActions('auth', ['login', 'logout']),
+  toggleShadow() {
+  // if page is scrolled
+    if (window.scrollY !== 0) {
+      this.$el.classList.add('Header--shadow');
+    } else {
+      this.$el.classList.remove('Header--shadow');
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.toggleShadow);
+  },
+
+};
+
+</script>
